7314a131bc24c2d56cc4d23c607834a56138ec60
Amanda D <amanda@depaula.xyz>
2020-09-17 14:22:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,7 @@ my_favorite_drinks = ["Coffee", "Wine", "Water", "Tea", "Zevia"]
 
 def loop_message_five_times(string)
   message = 0
-  while string message < array.length
+  while string message < array.length do
     puts string[message]
     message +=1
   end
